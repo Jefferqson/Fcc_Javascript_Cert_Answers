c03ddc911ea64382b28131c5929f1768bc74a8ee
Jefferqson <151244034+Jefferqson@users.noreply.github.com>
2023-12-12 16:57:48 -0500

    Update TelephoneValidator.js

--- TelephoneValidator.js
+++ TelephoneValidator.js
@@ -8,14 +8,3 @@ function telephoneCheck(str) {
   }
   
 console.log(telephoneCheck("5555555555"));
-
-
-possibleRegex = /^1?\s?(\(\d{3}\)\s?|\-?\d{3}\-|\d{3}\s?)\d{3}(\s?|-?)\d{4}$/
-
-console.log(possibleRegex.test("1 (555) 555-5555"))
-
-// first part -- ^1?\s?   Maybe a 1, maybe a space, maybe both, maybe neither
-// will i need to remove possibility of just a space?
-
-//second part -- \(\d{3}\)|\-?\d{3}\- either (###) or -###- or ###-
-// but I also need _###_
